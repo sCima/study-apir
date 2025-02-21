dc21b133856c22f1384454ba05eb21b8b3e8aaa6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 21:35:27 -0300

    Primeiro commit

---
+++ src/main/java/com/github/acnaweb/study_apir/StudyApirApplication.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.study_apir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/acnaweb/study_apir/StudyApirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.study_apir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
