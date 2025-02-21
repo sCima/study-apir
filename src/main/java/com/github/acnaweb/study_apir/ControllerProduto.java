3bacdc90f23a99d1bdc06ff2e10a73fca1e0ac0b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-20 22:47:06 -0300

    adicionada ControllerProduto

---
+++ src/main/java/com/github/acnaweb/study_apir/ControllerProduto.java
@@ -0,0 +1,36 @@
+package com.github.acnaweb.study_apir;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ControllerProduto {
+
+    @PostMapping
+    public ResponseEntity<String> create() {
+        return ResponseEntity.status(201).body("Produto cadastrado");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete() {
+        return ResponseEntity.status(204).build();
+
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update() {
+        return ResponseEntity.status(200).body("Produto atualizado");        
+    }
+
+    @GetMapping
+    public ResponseEntity<String> find() {
+        return ResponseEntity.status(200).body("Maça");        
+
+    }
+}
