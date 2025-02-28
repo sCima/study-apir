12ce1ca866d934f82fbfe089a98d2f50607202ed
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 22:52:37 -0300

    feat: add service

--- src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
@@ -1,5 +1,6 @@
 package com.github.acnaweb.study_apir.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,28 +9,37 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.acnaweb.study_apir.service.ProductService;
+
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduct {
 
+    @Autowired
+    private ProductService productService;
+
     @PostMapping
     public ResponseEntity<String> create() {
+        productService.createProduct(null);
         return ResponseEntity.status(201).body("Produto cadastrado");
     }
 
     @DeleteMapping
     public ResponseEntity<Void> delete() {
+        productService.deleteProduct(null);
         return ResponseEntity.status(204).build();
 
     }
 
     @PutMapping
     public ResponseEntity<String> update() {
+        productService.updateProduct(null, null);
         return ResponseEntity.status(200).body("Produto atualizado");        
     }
 
     @GetMapping
     public ResponseEntity<String> find() {
+        productService.getProductById(null);
         return ResponseEntity.status(200).body("Maça");        
 
     }
--- src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
+++ src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
@@ -2,8 +2,24 @@ package com.github.acnaweb.study_apir.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.github.acnaweb.study_apir.model.Product;
 
+@Service
 public class ProductService {
     private List<Product> products;
+
+    public Product createProduct(Product product) {
+        return null;
+    }
+    public Product getProductById(Long id) {
+        return null;
+    }
+    public Product updateProduct(Long id, Product product) {
+        return null;
+    }
+    public void deleteProduct(Long id) {
+
+    }
 }
