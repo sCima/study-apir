305d41e0f2bcde0e63d4a0fe8c7eb19e6dc4c403
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 21:00:57 -0300

    feat: add Products

--- src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduto.java
+++ src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduto.java
@@ -1,4 +1,4 @@
-package com.github.acnaweb.study_apir;
+package com.github.acnaweb.study_apir.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
